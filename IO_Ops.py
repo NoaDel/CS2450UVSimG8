1eeb8d8c5d52350eb6b587b5fc965f5c0eceb5c6
iGeoGeeki <156973901+iGeoGeeki@users.noreply.github.com>
2025-02-22 20:12:57 -0700

    Create IO_Ops.py
    Holds Input/Output Operations

---
+++ IO_Ops.py
@@ -0,0 +1,26 @@
+from output_handler import OutputHandler
+# I/O Operations
+class IO:
+    def read(memory, instruction_counter, operand):
+        # value = int(input("Enter an integer: ")) #Commenting this allows GUI to continue.
+        value = OutputHandler.get_int_input()
+        if -9999 <= value <= 9999:
+            memory[operand] = value
+        else:
+            OutputHandler.write_to_output("Error: Input out of range (-9999 to 9999).")
+            print("Error: Input out of range (-9999 to 9999).")
+            instruction_counter -= 1  # Re-execute
+
+        # Request integer input asynchronously
+        return instruction_counter
+
+    def write(memory, operand):
+        OutputHandler.write_to_output(f"Output: {memory[operand]}")
+        print("Output:", memory[operand])
+
+    # Load/Store Operations
+    def load(memory, operand):
+        return memory[operand]
+
+    def store(accum):
+        return accum
