0932f5de617a9470ef69dd0b888fad3433e28b76
Caden Brooks <caden@brooks.nu>
2025-03-05 03:12:20 -0700

    added color theme feature

--- GUI_code.py
+++ GUI_code.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, scrolledtext
-from GUI_settings import default_fonts, DefaultTheme, LightTheme, DarkTheme
+from GUI_settings import default_fonts, DefaultTheme, LightTheme, NeutralTheme, DarkTheme
 from UVSim import UVSim
 from output_handler import OutputHandler
 
@@ -11,6 +11,7 @@ def import_prog():
                 GUI.text_editor.delete('1.0', tk.END)
                 GUI.text_editor.insert('1.0', file.read())
     print("importing program...")
+    GUI.update_line_numbers()
 
 def save_prog():
     file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
@@ -35,6 +36,8 @@ def run_prog():
 
 class GUI():
     """Graphical User Interface class for UVSim."""
+
+    widgets = []
 
     def write_to_output(text: str):
         """Writes text to the output window."""
@@ -64,9 +67,11 @@ class GUI():
         new_button.config(command= lambda : GUI.focus_file(new_button))
         new_button.bind('<Enter>', lambda event: GUI.on_hover(event, new_button, GUI.theme.file_button_highlight_color))             
         new_button.bind('<Leave>', lambda event: GUI.on_leave(event, new_button))
+        new_button.bg = GUI.theme.file_button_color
+        new_button.fg = GUI.theme.text_color
+        GUI.widgets.append(new_button)
 
         GUI.file_buttons.append(new_button)
-
         for i, btn in enumerate(GUI.file_buttons):
             btn.grid(row=0, column=i, sticky="ns")
 
@@ -79,7 +84,7 @@ class GUI():
 
     def on_leave(event, button):
         """Restores button color when not hovered."""
-        button.config(bg=GUI.theme.menu_button_color)
+        button.config(bg=button.color)
 
     def clear_output():
         GUI.output_box.config(state=tk.NORMAL)
@@ -89,16 +94,44 @@ class GUI():
     
     def change_theme(name, button):
         if name == LightTheme.name:
-            GUI.theme = LightTheme()
+            current_theme = LightTheme
+        elif name == NeutralTheme.name:
+            current_theme = NeutralTheme
         elif name == DarkTheme.name:
-            GUI.theme = DarkTheme()
+            current_theme = DarkTheme
         else:
-            GUI.theme = DefaultTheme()
+            current_theme = DefaultTheme
+        
+        GUI.theme.name = current_theme.name
+        # Background colors
+        GUI.theme.header_color[0] = current_theme.header_color
+        GUI.theme.file_header_color[0] = current_theme.file_header_color
+        GUI.theme.editor_color[0] = current_theme.editor_color
+        GUI.theme.line_num_color[0] = current_theme.line_num_color
+        GUI.theme.output_color[0] = current_theme.output_color
+        GUI.theme.setting_content_color[0] = current_theme.setting_content_color
+        # Button colors
+        GUI.theme.menu_button_color[0] = current_theme.menu_button_color
+        GUI.theme.menu_button_highlight_color[0] = current_theme.menu_button_highlight_color
+        GUI.theme.file_button_color[0] = current_theme.file_button_color
+        GUI.theme.file_button_highlight_color[0] = current_theme.file_button_highlight_color
+        GUI.theme.file_focus_color[0] = current_theme.file_focus_color
+        # Text color
+        GUI.theme.text_color[0] = current_theme.text_color
 
         for btn in GUI.theme_buttons:
             btn.config(font=default_fonts.setting_font)
         button.config(font=default_fonts.setting_selected_font)
 
+        for widget in GUI.widgets:
+            try:  # try blocks required because some widgets dont have a foreground or cursor
+                widget.config(bg=widget.bg, fg=widget.fg, insertbackground=widget.cursor)
+            except:
+                try:
+                    widget.config(bg=widget.bg, fg=widget.fg)
+                except:
+                    widget.config(bg=widget.bg)
+            
     def create_layout_frames(window):
         """Creates the main layout frames (header, editor, terminal)."""
 
@@ -116,46 +149,68 @@ class GUI():
 
         menu_header = tk.Frame(GUI.main_frame, bg=GUI.theme.header_color)
         menu_header.place(relx=0,rely=0,relwidth=1, relheight=.03)
+        menu_header.bg = GUI.theme.header_color
+        GUI.widgets.append(menu_header)
 
         GUI.file_header = tk.Frame(GUI.main_frame, bg=GUI.theme.file_header_color)
         GUI.file_header.place(relx=0,rely=.03,relwidth=1, relheight=.03)
+        GUI.file_header.bg = GUI.theme.file_header_color
+        GUI.widgets.append(GUI.file_header)
 
         editor = tk.Frame(GUI.main_frame, bg=GUI.theme.editor_color)
         editor.place(relx=0,rely=.06,relwidth=1, relheight=.57)
+        editor.bg = GUI.theme.editor_color
+        GUI.widgets.append(editor)
 
         terminal = tk.Frame(GUI.main_frame, bg=GUI.theme.output_color)
         terminal.place(relx=0,rely=.63,relwidth=1, relheight=.4)
+        terminal.bg = GUI.theme.output_color
+        GUI.widgets.append(terminal)
 
         return menu_header, GUI.file_header, editor, terminal, setting_header, settings_content_frame
 
     def create_menu_header(frame):
         """Creates the header menu with Import, Save, and Run buttons."""
-        import_button = tk.Button(frame, text="Import", command= lambda: import_prog(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="dark grey")
-        import_button.place(relx=0,rely=0,relwidth=.05, relheight=1)
+        import_button = tk.Button(frame, text=" Import ", command= lambda: import_prog(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="dark grey")
+        import_button.grid(row=1,column=1)
+        import_button.bg = GUI.theme.menu_button_color
         import_button.bind('<Enter>', lambda event: GUI.on_hover(event, import_button, GUI.theme.menu_button_highlight_color))             
         import_button.bind('<Leave>', lambda event: GUI.on_leave(event, import_button))
+        import_button.bg = GUI.theme.menu_button_color
+        import_button.fg = GUI.theme.text_color
+        GUI.widgets.append(import_button)
 
-        save_button = tk.Button(frame, text="Save", command= lambda: save_prog(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="grey")
-        save_button.place(relx=.05,rely=0,relwidth=.05, relheight=1)
+        save_button = tk.Button(frame, text=" Save ", command= lambda: save_prog(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="grey")
+        save_button.grid(row=1,column=2)
         save_button.bind('<Enter>', lambda event: GUI.on_hover(event, save_button, GUI.theme.menu_button_highlight_color))             
         save_button.bind('<Leave>', lambda event: GUI.on_leave(event, save_button))
+        save_button.bg = GUI.theme.menu_button_color
+        save_button.fg = GUI.theme.text_color
+        GUI.widgets.append(save_button)
 
-        New_file_button = tk.Button(frame, text="New", command= lambda: GUI.create_new_file(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="grey")
-        New_file_button.place(relx=.1,rely=0,relwidth=.05, relheight=1)
+        New_file_button = tk.Button(frame, text=" New ", command= lambda: GUI.create_new_file(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="grey")
+        New_file_button.grid(row=1,column=3)
         New_file_button.bind('<Enter>', lambda event: GUI.on_hover(event, New_file_button, GUI.theme.menu_button_highlight_color))             
         New_file_button.bind('<Leave>', lambda event: GUI.on_leave(event, New_file_button))
+        New_file_button.bg = GUI.theme.menu_button_color
+        New_file_button.fg = GUI.theme.text_color
+        GUI.widgets.append(New_file_button)
 
-        New_file_button.config(state=tk.DISABLED)
-
-        setting_button = tk.Button(frame, text="Settings", command= lambda: GUI.focus_setting_window(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="light grey")
-        setting_button.place(relx=.15,rely=0,relwidth=.08, relheight=1)
+        setting_button = tk.Button(frame, text=" Settings " , command= lambda: GUI.focus_setting_window(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="light grey")
+        setting_button.grid(row=1,column=4)
         setting_button.bind('<Enter>', lambda event: GUI.on_hover(event, setting_button, GUI.theme.menu_button_highlight_color))             
         setting_button.bind('<Leave>', lambda event: GUI.on_leave(event, setting_button))
+        setting_button.bg = GUI.theme.menu_button_color
+        setting_button.fg = GUI.theme.text_color
+        GUI.widgets.append(setting_button)
 
-        run_button = tk.Button(frame, text="Run", command= lambda: run_prog(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="light grey")
-        run_button.place(relx=.23,rely=0,relwidth=.05, relheight=1)
+        run_button = tk.Button(frame, text=" Run ", command= lambda: run_prog(), fg=GUI.theme.text_color, font=default_fonts.menu_font, bg=GUI.theme.menu_button_color, bd=0, activebackground="light grey")
+        run_button.grid(row=1,column=5)
         run_button.bind('<Enter>', lambda event: GUI.on_hover(event, run_button, GUI.theme.menu_button_highlight_color))             
         run_button.bind('<Leave>', lambda event: GUI.on_leave(event, run_button))
+        run_button.bg = GUI.theme.menu_button_color
+        run_button.fg = GUI.theme.text_color
+        GUI.widgets.append(run_button)
 
     def create_file_header(file_header):
         GUI.file_buttons = []
@@ -169,10 +224,17 @@ class GUI():
             line_numbers.yview(*args)
 
         # Create a text box widget
-        GUI.text_editor = tk.Text(frame, wrap=tk.WORD, font=default_fonts.editor_font, fg=GUI.theme.text_color, undo=True, bg=GUI.theme.editor_color, bd=0)
+        GUI.text_editor = tk.Text(frame, wrap=tk.WORD, insertbackground=GUI.theme.text_color,font=default_fonts.editor_font, fg=GUI.theme.text_color, undo=True, bg=GUI.theme.editor_color, bd=0)
+        GUI.text_editor.bg = GUI.theme.editor_color
+        GUI.text_editor.fg = GUI.theme.text_color
+        GUI.text_editor.cursor = GUI.theme.text_color
+        GUI.widgets.append(GUI.text_editor)
         
          # Create a line number text box
         line_numbers = tk.Text(frame, wrap=tk.WORD, font=default_fonts.line_num_font, fg=GUI.theme.text_color, width=4, padx=5, takefocus=0, state=tk.DISABLED, bg=GUI.theme.line_num_color, bd=0)
+        line_numbers.bg = GUI.theme.line_num_color
+        line_numbers.fg = GUI.theme.text_color
+        GUI.widgets.append(line_numbers)
 
         # Add a scrollbar widget to the Text widget
         scrollbar = tk.Scrollbar(frame, command=on_scroll)
@@ -183,8 +245,17 @@ class GUI():
         Input_frame = tk.Frame(frame, bg=GUI.theme.editor_color,width=90, padx=5,)
         
         # ADD a input widgets
-        GUI.input_box = tk.Text(Input_frame, wrap=tk.WORD, font=default_fonts.editor_font, fg=GUI.theme.text_color, undo=True, bg=GUI.theme.editor_color, bd=0)
+        GUI.input_box = tk.Text(Input_frame, wrap=tk.WORD, insertbackground=GUI.theme.text_color,font=default_fonts.editor_font, fg=GUI.theme.text_color, undo=True, bg=GUI.theme.editor_color, bd=0)
+        GUI.input_box.bg = GUI.theme.editor_color
+        GUI.input_box.fg = GUI.theme.text_color
+        GUI.input_box.cursor = GUI.theme.text_color
+        GUI.widgets.append(GUI.input_box)
+
         input_label = tk.Label(Input_frame, text="(each input\nmust be on\na new line)\n\ninputs:", bg=GUI.theme.editor_color, bd=0)
+        input_label.bg = GUI.theme.editor_color
+        input_label.fg = GUI.theme.text_color
+        GUI.widgets.append(input_label)
+
         # Pack all the widgets
         line_numbers.pack(side=tk.LEFT, fill=tk.Y)
         GUI.text_editor.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
@@ -215,7 +286,10 @@ class GUI():
 
         # Create a text box widget (output window)
         GUI.output_box = tk.Text(frame, wrap=tk.WORD, fg=GUI.theme.text_color, font=default_fonts.output_font, state=tk.DISABLED, bg=GUI.theme.output_color, bd=0)
-        
+        GUI.output_box.bg = GUI.theme.output_color
+        GUI.output_box.fg = GUI.theme.text_color
+        GUI.widgets.append(GUI.output_box)
+
         # Add a scrollbar
         scrollbar = tk.Scrollbar(frame, command=GUI.output_box.yview)
         GUI.output_box.config(yscrollcommand=scrollbar.set)
@@ -224,19 +298,20 @@ class GUI():
         GUI.output_box.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        # Function to write text to the output box
-        
         GUI.write_to_output("Output:")
         
         GUI.trash_img = tk.PhotoImage(file="trash.png")
         # trash_button = tk.Button(frame, text="🗑", font=("Arial", 20), padx=0, pady=0, command=GUI.clear_output)
         trash_button = tk.Button(GUI.output_box, image=GUI.trash_img, bg=GUI.theme.output_color, activebackground="grey", bd=0, command=GUI.clear_output)
+        trash_button.bg = GUI.theme.output_color
+        trash_button.fg = GUI.theme.text_color
+        GUI.widgets.append(trash_button)
         trash_button.place(relx=1.0, x=-30, rely=0.01)
     
     def create_setting_header(frame):
         save_n_exit_button = tk.Button(frame, text="Save & Exit",bd=0,bg=GUI.theme.menu_button_color, fg=GUI.theme.text_color, command=GUI.focus_main_window)
         save_n_exit_button.place(relx=0,rely=0,relwidth=.1, relheight=1)
-        save_n_exit_button.bind('<Enter>', lambda event: GUI.on_hover(event, save_n_exit_button, GUI.theme.file_button_highlight_color))             
+        save_n_exit_button.bind('<Enter>', lambda event: GUI.on_hover(event, save_n_exit_button, GUI.theme.menu_button_highlight_color))             
         save_n_exit_button.bind('<Leave>', lambda event: GUI.on_leave(event, save_n_exit_button))
 
     def create_setting_window(frame):
@@ -246,7 +321,7 @@ class GUI():
         setting_theme_lbl = tk.Label(setting_theme_frame, text="Theme: ", font=('Helvatical bold',int(25)), bg=GUI.theme.setting_content_color,fg = 'black', pady = 10)
         setting_theme_lbl.pack(side=tk.LEFT)
 
-        default_theme_btn = tk.Button(setting_theme_frame, text = 'default', width=int(9), height=int(.5),pady = 10,font=(default_fonts.setting_selected_font if GUI.theme.name == "default_theme" else default_fonts.setting_font))
+        default_theme_btn = tk.Button(setting_theme_frame, text = 'default (UVU)', width=int(11), height=int(.5),pady = 10,font=(default_fonts.setting_selected_font if GUI.theme.name == "default_theme" else default_fonts.setting_font))
         default_theme_btn.config(command=lambda: GUI.change_theme("default_theme", default_theme_btn))
         default_theme_btn.pack(side=tk.RIGHT, padx=(5,20))
         
@@ -254,19 +329,16 @@ class GUI():
         light_theme_btn.config(command=lambda: GUI.change_theme("light_theme", light_theme_btn))
         light_theme_btn.pack(side=tk.RIGHT, padx=(5,20))
         
+        neutral_theme_btn = tk.Button(setting_theme_frame, text = 'neutral', width=int(9), height=int(.5),pady = 10,font=(default_fonts.setting_selected_font if GUI.theme.name == "neutral_theme" else default_fonts.setting_font))
+        neutral_theme_btn.config(command=lambda: GUI.change_theme("neutral_theme", neutral_theme_btn))
+        neutral_theme_btn.pack(side=tk.RIGHT, padx=(5,20))
+
         dark_theme_btn = tk.Button(setting_theme_frame, text = 'dark', width=int(9), height=int(.5), pady = 10,font=(default_fonts.setting_selected_font if GUI.theme.name == "dark_theme" else default_fonts.setting_font))
         dark_theme_btn.config(command=lambda: GUI.change_theme("dark_theme", dark_theme_btn))
         dark_theme_btn.pack(side=tk.RIGHT, padx=(5,20))
 
-        default_theme_btn.config(state=tk.DISABLED)
-        light_theme_btn.config(state=tk.DISABLED)
-        dark_theme_btn.config(state=tk.DISABLED)
-
-        GUI.theme_buttons = []
-        GUI.theme_buttons.append(default_theme_btn)
-        GUI.theme_buttons.append(light_theme_btn)
-        GUI.theme_buttons.append(dark_theme_btn)
-
+        GUI.theme_buttons = [default_theme_btn, light_theme_btn, neutral_theme_btn, dark_theme_btn]
+    
 def main():
     """Main function to initialize and run the Tkinter GUI."""
     root = tk.Tk() 
--- GUI_settings.py
+++ GUI_settings.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 class default_fonts():
     """ font=("FontName", Size, "Style") """
     # style include: "bold", "underline", "italic", "underline", "overstrike"
-    menu_font = ('Helvatical',10)
+    menu_font = ('Helvatical',9)
     file_header_font = ("Arial", 12)
     editor_font = ("Arial", 12)
     line_num_font = ("Arial", 12)
@@ -17,7 +17,55 @@ def rgb_2_hex(r, g, b):
 
 class DefaultTheme():
     """ Format: (red, green, blue) ranging from 0-256 """
+
+    # UVU dark green = (76,114,29) 
+    # UVU white (255,255,255) as the off-color
+
     name = "default_theme"
+
+    # Background colors
+    header_color = rgb_2_hex(76,114,29)
+    file_header_color = rgb_2_hex(230, 230, 230)
+    editor_color = rgb_2_hex(255, 255, 255)
+    line_num_color = rgb_2_hex(220, 230, 220)
+    output_color = rgb_2_hex(200, 210, 200)
+    setting_content_color = rgb_2_hex(220, 230, 220)
+
+    # Button colors
+    menu_button_color = rgb_2_hex(76,114,29)
+    menu_button_highlight_color = rgb_2_hex(96,134,49)
+
+    file_button_color = rgb_2_hex(220, 220, 220)
+    file_button_highlight_color = rgb_2_hex(180, 180, 180)
+    file_focus_color = rgb_2_hex(255, 255, 255)
+
+    # Text color
+    text_color = rgb_2_hex(0, 0, 0)
+
+    def __init__(self):
+        """color MUST be initialized as a list to make them mutable"""
+        # Background colors
+        self.header_color = [rgb_2_hex(76,114,29)]
+        self.file_header_color = [rgb_2_hex(230, 230, 230)]
+        self.editor_color = [rgb_2_hex(255, 255, 255)]
+        self.line_num_color = [rgb_2_hex(220, 230, 220)]
+        self.output_color = [rgb_2_hex(200, 210, 200)]
+        self.setting_content_color = [rgb_2_hex(180, 180, 180)]
+
+        # Button colors
+        self.menu_button_color = [rgb_2_hex(76,114,29)]
+        self.menu_button_highlight_color = [rgb_2_hex(96,134,49)]
+
+        self.file_button_color = [rgb_2_hex(220, 220, 220)]
+        self.file_button_highlight_color = [rgb_2_hex(180, 180, 180)]
+        self.file_focus_color = [rgb_2_hex(255, 255, 255)]
+
+        # Text color
+        self.text_color = [rgb_2_hex(0, 0, 0)]
+
+class NeutralTheme(DefaultTheme):
+    """ Format: (red, green, blue) ranging from 0-256 """
+    name = "neutral_theme"
 
     # Background colors
     header_color = rgb_2_hex(150, 150, 150)
@@ -29,15 +77,14 @@ class DefaultTheme():
 
     # Button colors
     menu_button_color = rgb_2_hex(150, 150, 150)
-    menu_button_highlight_color = rgb_2_hex(128, 100, 100)
+    menu_button_highlight_color = rgb_2_hex(100, 100, 100)
 
     file_button_color = rgb_2_hex(170, 170, 170)
-    file_button_highlight_color = rgb_2_hex(128, 100, 100)
+    file_button_highlight_color = rgb_2_hex(100, 100, 100)
     file_focus_color = rgb_2_hex(200, 200, 200)
 
     # Text color
     text_color = rgb_2_hex(0, 0, 0)
-
 
 class LightTheme(DefaultTheme):
     """ Light theme colors with slightly brighter shades """
@@ -49,33 +96,32 @@ class LightTheme(DefaultTheme):
     line_num_color = rgb_2_hex(230, 230, 230)
     output_color = rgb_2_hex(180, 180, 180)
 
-    menu_button_color = rgb_2_hex(200, 200, 200)
-    menu_button_highlight_color = rgb_2_hex(180, 160, 160)
+    menu_button_color = rgb_2_hex(220, 220, 220)
+    menu_button_highlight_color = rgb_2_hex(160, 160, 160)
 
-    file_button_color = rgb_2_hex(210, 210, 210)
-    file_button_highlight_color = rgb_2_hex(180, 160, 160)
-    file_focus_color = rgb_2_hex(250, 250, 250)
+    file_button_color = rgb_2_hex(230, 230, 230)
+    file_button_highlight_color = rgb_2_hex(180, 180, 180)
+    file_focus_color = rgb_2_hex(255, 255, 255)
     
     # Text color
     text_color = rgb_2_hex(0, 0, 0)
-
 
 class DarkTheme(DefaultTheme):
     """ Dark theme colors with deeper, darker shades """
     name = "dark_theme"
 
     header_color = rgb_2_hex(50, 50, 50)
-    file_header_color = rgb_2_hex(70, 70, 70)
-    editor_color = rgb_2_hex(40, 40, 40)
+    file_header_color = rgb_2_hex(55, 55, 55)
+    editor_color = rgb_2_hex(80, 80, 80)
     line_num_color = rgb_2_hex(60, 60, 60)
-    output_color = rgb_2_hex(30, 30, 30)
+    output_color = rgb_2_hex(50, 50, 50)
 
-    menu_button_color = rgb_2_hex(80, 80, 80)
-    menu_button_highlight_color = rgb_2_hex(100, 70, 70)
+    menu_button_color = rgb_2_hex(50, 50, 50)
+    menu_button_highlight_color = rgb_2_hex(100, 100, 100)
 
-    file_button_color = rgb_2_hex(90, 90, 90)
-    file_button_highlight_color = rgb_2_hex(100, 70, 70)
-    file_focus_color = rgb_2_hex(60, 60, 60)
+    file_button_color = rgb_2_hex(70, 70, 75)
+    file_button_highlight_color = rgb_2_hex(100, 100, 100)
+    file_focus_color = rgb_2_hex(80, 80, 80)
     
     # Text color
     text_color = rgb_2_hex(200, 200, 200)
