a4c998845451de8d72b945eeee16401398a16ba1
iGeoGeeki <156973901+iGeoGeeki@users.noreply.github.com>
2025-03-21 02:52:33 -0600

    Update IO_Ops read()
    This detects errors found during runtime and moves instruction counter immediately to the halt command.

--- IO_Ops.py
+++ IO_Ops.py
@@ -1,19 +1,19 @@
 from output_handler import OutputHandler
-import asyncio
-import concurrent.futures
+
 # I/O Operations
 class IO:
     def read(memory, instruction_counter, operand):
         # value = int(input("Enter an integer: ")) #Commenting this allows GUI to continue.
         # value = OutputHandler.get_int_input()
         value = OutputHandler.input_vals.pop(0)
-        
+
         if -9999 <= value <= 9999:
             memory[operand] = value
         else:
-            OutputHandler.write_to_output("Error: Input out of range (-9999 to 9999).")
+            OutputHandler.write_to_output("ERROR: Input out of accepting range -9999 to 9999. Program halted.")
             print("Error: Input out of range (-9999 to 9999).")
-            instruction_counter -= 1  # Re-execute
+            while memory[instruction_counter] != '0': #Moves program to end without reading any other instructions when this error occurs.
+                instruction_counter += 1
 
         # Request integer input asynchronously
         return instruction_counter
