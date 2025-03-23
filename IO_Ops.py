bc461e762b3f16d659cfbdce67e9a31cb306d1d7
iGeoGeeki <156973901+iGeoGeeki@users.noreply.github.com>
2025-03-22 21:50:43 -0600

    Updated read() fo terminal

--- IO_Ops.py
+++ IO_Ops.py
@@ -5,15 +5,29 @@ class IO:
     def read(memory, instruction_counter, operand):
         # value = int(input("Enter an integer: ")) #Commenting this allows GUI to continue.
         # value = OutputHandler.get_int_input()
-        value = OutputHandler.input_vals.pop(0)
-
-        if -9999 <= value <= 9999:
-            memory[operand] = value
+        if not OutputHandler.via_terminal:
+            print('before')
+            value = OutputHandler.input_vals.pop(0)
+            print('after')
+            if -9999 <= value <= 9999:
+                memory[operand] = value
+            else:
+                OutputHandler.write_to_output("ERROR: Input out of accepting range -9999 to 9999. Program halted.")
+                print("Error: Input out of range (-9999 to 9999).")
+                while memory[instruction_counter] != '0':
+                    instruction_counter += 1  # Re-execute
+                instruction_counter -= 1
         else:
-            OutputHandler.write_to_output("ERROR: Input out of accepting range -9999 to 9999. Program halted.")
-            print("Error: Input out of range (-9999 to 9999).")
-            while memory[instruction_counter] != '0': #Moves program to end without reading any other instructions when this error occurs.
-                instruction_counter += 1
+            while True:
+                try:
+                    value = int(input("Enter an integer: "))
+                    if -9999 <= value <= 9999:
+                        memory[operand] = value
+                        break
+                    else:
+                        print("Error: Input out of range (-9999 to 9999).")
+                except:
+                    print("Error: Invalid input type.")
 
         # Request integer input asynchronously
         return instruction_counter
